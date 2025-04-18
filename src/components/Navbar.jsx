2df03d6df117b14589dfb884d68b62cc9f3f9cfb
yerranedi srinu <srinu427452@gmail.com>
2025-04-18 15:14:09 +0530

    navbar component completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import{NavLink} from 'react-router-dom'
 import{assets} from'../assets/assets'
-
+import{useAppContext}from"../context/AppContext"
 
  const Navbar = () => {
     const [open, setOpen] = React.useState(false)
+    const{user,setUser,setShowUserLogin, navigate}=useAppContext()
+
+    const logout=async()=>{
+        setUser(null);
+        navigate('/')
+    }
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
  
-    <NavLink  to="/">
+    <NavLink  to="/" onClick={()=>setOpen(false)}>
         <img className="h-9" src={assets.logo} alt="Logo" />
     </NavLink>
 
@@ -23,37 +29,57 @@ import{assets} from'../assets/assets'
             <img src={assets.search_icon} alt='search' className='w-5 h-5'   />
         </div>
 
-        <div className="relative cursor-pointer">
-            <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
+        <div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
+            <img src={assets.nav_cart_icon} alt='Cart' className='w-6 opacity-80' />
             <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
         </div>
 
-        <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+        {!user ?(<button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
             Login
-        </button>
+        </button>)
+        :
+        (
+         <div className='relative group'>
+            <img src={assets.profile_icon} className='w-10' alt='' />
+            <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow 
+            border border-gray-200 py-2.5 w30 rounded-md text-sm z-40'>
+                <li onClick={()=>navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>MyOrders</li>
+                <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+            </ul>
+         </div>   
+        )}
     </div>
 
     <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
         {/* Menu Icon SVG */}
-        <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="21" height="1.5" rx=".75" fill="#426287" />
-            <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
-            <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287" />
-        </svg>
+        <img src={assets.menu_icon} alt='Menu'   />
     </button>
 
     {/* Mobile Menu */}
+    {open &&(
     <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-        <a href="#" className="block">Home</a>
-        <a href="#" className="block">About</a>
-        <a href="#" className="block">Contact</a>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+        <NavLink to='/' onClick={()=>setOpen(false)}>Home</NavLink>
+        <NavLink to='/products' onClick={()=>setOpen(false)}>All Products</NavLink>
+        {user &&
+        <NavLink to='/products' onClick={()=>setOpen(false)}>My Orders</NavLink>
+        }
+
+        <NavLink to='/' onClick={()=>setOpen(false)}>Contact</NavLink>
+        {!user ? (
+            <button onClick={()=>{
+                setOpen(false);
+                setShowUserLogin(true);
+            }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
             Login
         </button>
+        ):(
+            <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+            Logout
+        </button>
+        )}
+        
     </div>
-
+    )}
 </nav>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,10 +6,14 @@ export const AppContext = createContext();
 export const AppContectProvider=({children})=>{
 
     const navigate =useNavigate();
-    const [user, setUser]=useState(null)
+    const [user, setUser]=useState(null) 
     const [isSeller, setIsSeller]=useState(false)
+    const [ishowUserLogin, setShowUserLogn]=useState(false)
 
-    const value={navigate,user,setIsSeller,setUser,isSeller}
+
+    const value={navigate,user,setIsSeller,setUser,isSeller,
+        ishowUserLogin,setShowUserLogn
+    }
     
     return <AppContext.Provider value={value}>
         {children}
