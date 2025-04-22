df8e904225562c51daaac6d694be1b597fa4aab1
Sinu8280 <hrssrconcrete85@gmail.com>
2025-04-22 18:03:51 +0530

    Home page completed duration 2:8:23

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import NewsLetters from '../components/NewsLetters'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner/>
         <Categories />  
-         <BestSeller/> 
+         <BestSeller/>
+         <BottomBanner/> 
+         <NewsLetters/>
+         <Footer/>
     </div>
   )
 }
