b9a7df3bc61078362b2a428d71ff76ef0035e17b
Sinu8280 <hrssrconcrete85@gmail.com>
2025-04-23 12:23:22 +0530

    Home page completed duration 02:16:17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
 import NewsLetters from '../components/NewsLetters'
-import Footer from '../components/Footer'
+
 
 const Home = () => {
   return (
@@ -14,7 +14,7 @@ const Home = () => {
          <BestSeller/>
          <BottomBanner/> 
          <NewsLetters/>
-         <Footer/>
+         
     </div>
   )
 }
